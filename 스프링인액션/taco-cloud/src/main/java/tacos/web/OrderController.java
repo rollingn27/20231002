2f19fe178c80a81894f47bd58eb804d807ea456c
rollingn27 <106942510+rollingn27@users.noreply.github.com>
2023-10-03 00:27:02 +0900

    [스프링인액션]chap03

--- 스프링인액션/taco-cloud/src/main/java/tacos/web/OrderController.java
+++ 스프링인액션/taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -1,5 +1,6 @@
 package tacos.web;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,14 +8,21 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import tacos.Order;
+import tacos.data.OrderRepository;
 
 import javax.validation.Valid;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderRepository orderRepository;
 
     @GetMapping("/current")
     public String orderForm(Model model) {
@@ -23,13 +31,14 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors) {
+    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
 
         if (errors.hasErrors()) {
             return "orderForm";
         }
 
-        log.info("Order submitted: " + order);
-        return "redirect:";
+        orderRepository.save(order);
+        sessionStatus.setComplete();
+        return "redirect:/";
     }
 }
