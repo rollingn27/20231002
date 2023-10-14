4d3122ddf1dcc89f1a36e3b97ee90ecafe31ba72
rollingn27 <arollingstone27@gmail.com>
2023-10-14 17:49:31 +0900

    [리액트모던웹개발] ch05_1

---
+++ 리액트모던웹개발/ch05/ch05_1/src/pages/UseReducerClock.tsx
@@ -0,0 +1,31 @@
+import { useReducer } from 'react';
+import type { AppState } from '../store';
+import type { SetTodayAction } from '../store/actions';
+import { Div, Title, Subtitle } from '../components';
+import { useInterval } from '../hooks';
+
+export default function UseReduxClock() {
+  const [{ today }, dispatch] = useReducer(
+    (state: AppState, action: SetTodayAction) => {
+      switch (action.type) {
+        case 'setToday':
+          return { ...state, today: new Date() };
+      }
+    },
+    {
+      today: new Date(),
+    }
+  );
+
+  useInterval(() => {
+    dispatch({ type: 'setToday', today: new Date() });
+  });
+
+  return (
+    <Div className="flex-col items-center justify-center mt-16">
+      <Title className="text-5xl">UseReduxClock</Title>
+      <Title className="mt-4 text-3xl">{today.toLocaleTimeString()}</Title>
+      <Subtitle className="mt-4 text-2xl">{today.toLocaleDateString()}</Subtitle>
+    </Div>
+  );
+}
