50776dbb3e1aac294eb388df83a7a920b57016a0
rollingn27 <arollingstone27@gmail.com>
2023-10-23 20:51:41 +0900

    [리액트모던웹개발] ch06_2

---
+++ 리액트모던웹개발/ch06/ch06_2/src/routes/Layout/NavigationBar.tsx
@@ -0,0 +1,14 @@
+import { Link } from '../../components';
+
+export default function NavigationBar() {
+  return (
+    <div className="flex p-4 bg-gray-200">
+      <Link to="/" className="btn btn-link">
+        Home
+      </Link>
+      <Link to="/board" className="ml-4 btn btn-link">
+        Board
+      </Link>
+    </div>
+  );
+}
